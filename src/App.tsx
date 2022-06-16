986f64d8a833edeb4bc1700ce4a5b04c22ed9175
Alistair Taylor <alistairwilliamtaylor@gmail.com>
2022-06-16 16:09:34 +1000

    calls to unsplash API are now working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [searchKeyword, updateSearchKeyword] = useState("");
+
+  const [photos, updatePhotos] = useState(["hello"]);
+
+  const searchForImages = () => {
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: {
+          query: searchKeyword,
+          client_id: process.env.REACT_APP_UNSPLASH_ACCESS_KEY,
+        },
+      })
+      .then(
+        (response) => {
+          console.log(response);
+          const whatIWant = response.data.results.map(
+            (info: any) => info.urls.small
+          );
+          updatePhotos(whatIWant);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={searchKeyword}
+        onChange={(e) => updateSearchKeyword(e.target.value)}
+      />
+      <button onClick={() => searchForImages()}>Show me the pictures!</button>
+      <section className="photo-gallery">
+        {photos.map((photo) => {
+          return <img src={photo} />;
+        })}
+      </section>
     </div>
   );
 }
