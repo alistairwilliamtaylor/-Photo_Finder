5f7afc4d70f175ceaec58c5dc45d59b7c448b33a
Alistair Taylor <alistairwilliamtaylor@gmail.com>
2022-06-20 11:26:27 +1000

    added CSS grid and styled the page, generally reflects current levels of CSS understanding

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import "./PhotoGallery.css";
+import "./GalleryImage.css";
+import "./SearchPrompt.css";
 import axios from "axios";
 
-const numberOfImages = 9;
+const NUMBEROFIMAGES = 9;
 
-interface APIPhotoResultsData{
-  id: string,
-  alt_description: string,
+interface APIPhotoResultsData {
+  id: string;
+  alt_description: string;
   urls: {
-    small: string,
-  }
+    small: string;
+  };
 }
 
-interface PhotoInfo{
-    id: string,
-    altText: string,
-    url: string,
+interface PhotoInfo {
+  id: string;
+  altText: string;
+  url: string;
 }
 
 function App() {
@@ -28,33 +31,35 @@ function App() {
   };
 
   useEffect(() => {
-    getInitialRandomImages()
-  }, [])
-  
+    getInitialRandomImages();
+  }, []);
+
   const getInitialRandomImages = () => {
     axios
       .get("https://api.unsplash.com/photos/random", {
         params: {
           client_id: process.env.REACT_APP_UNSPLASH_ACCESS_KEY,
-          count: numberOfImages,
+          count: NUMBEROFIMAGES,
         },
       })
       .then(
         (response) => {
-          const initialPhotos = response.data.map((photoInfo: APIPhotoResultsData) => {
-            return {
-              id: photoInfo.id,
-              altText: photoInfo.alt_description,
-              url: photoInfo.urls.small,
-            };
-          });
+          const initialPhotos = response.data.map(
+            (photoInfo: APIPhotoResultsData) => {
+              return {
+                id: photoInfo.id,
+                altText: photoInfo.alt_description,
+                url: photoInfo.urls.small,
+              };
+            }
+          );
           updatePhotos(initialPhotos);
         },
         (error) => {
           console.log(error);
         }
       );
-  }
+  };
 
   const [photos, updatePhotos] = useState<PhotoInfo[]>([]);
 
@@ -64,19 +69,21 @@ function App() {
         params: {
           query: searchKeyword,
           client_id: process.env.REACT_APP_UNSPLASH_ACCESS_KEY,
-          per_page: numberOfImages,
+          per_page: NUMBEROFIMAGES,
         },
       })
       .then(
         (response) => {
           console.log(response);
-          const newPhotos = response.data.results.map((photoInfo: APIPhotoResultsData) => {
-            return {
-              id: photoInfo.id,
-              altText: photoInfo.alt_description,
-              url: photoInfo.urls.small,
-            };
-          });
+          const newPhotos = response.data.results.map(
+            (photoInfo: APIPhotoResultsData) => {
+              return {
+                id: photoInfo.id,
+                altText: photoInfo.alt_description,
+                url: photoInfo.urls.small,
+              };
+            }
+          );
           updatePhotos(newPhotos);
         },
         (error) => {
@@ -87,21 +94,30 @@ function App() {
 
   return (
     <div className="App">
-      <h1>{numberOfImages} Images</h1>
-      <label htmlFor="image-search-input">Show me {numberOfImages} images of:</label>
-      <input
-        type="text"
-        id="image-search-input"
-        value={searchKeyword}
-        onChange={(e) => updateSearchKeyword(e.target.value)}
-        onKeyDown={handleKeyDown}
-      />
-      <button onClick={() => searchForImages()}>Show me the pictures!</button>
-      <section className="photo-gallery">
-        {photos.map((photo) => {
-          return <img src={photo.url} key={photo.id} alt={photo.altText} />;
-        })}
-      </section>
+      <div className="content-container">
+        <h1>{NUMBEROFIMAGES} images</h1>
+        <label htmlFor="image-search-input">show me images of :</label>
+        <input
+          type="text"
+          id="image-search-input"
+          value={searchKeyword}
+          onChange={(e) => updateSearchKeyword(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <button onClick={() => searchForImages()}>go!</button>
+        <section className="photo-gallery">
+          {photos.map((photo) => {
+            return (
+              <img
+                src={photo.url}
+                key={photo.id}
+                alt={photo.altText}
+                className="gallery-image"
+              />
+            );
+          })}
+        </section>
+      </div>
     </div>
   );
 }
